fa3d17a750e0f33b56421b35c5ccd5b3cf7de5b1
san4ouzxc <shuriklishenko2@gmail.com>
2024-05-21 08:59:21 +0300

    abc

---
+++ zxc.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib> 
+#include <windows.h> 
+
+using namespace std;
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    for (int i = 0; i < 50; i++) {
+        cout << "НЕВОЗМОЖНО  ВСЕ ПЯТЕРО В ПОТОКЕ???  Это не" << endl;
+        cout << "идеальный Поток 😨. Их силы максимально приближены к " << endl;
+        cout << "Потоку благодаря Акаши💪😈. Это возможно, потому что у" << endl;
+        cout << "всех пятерых королей(zxcursit,uNike,akumaqwe, ыр,avice)" << endl;
+        cout << "достаточно таланта😱😱. clap Clap clAP CLAP👏👏👏" << endl;
+        cout << endl;
+        cout << "####  ####  ####  #### " << endl;
+        cout << "#     #  #  #  #  #    " << endl;
+        cout << "####  ####  ####  #### " << endl;
+        cout << "#  #     #     #  #  # " << endl;
+        cout << "####  ####  ####  #### " << endl;
+        system("cls");
+    }
+    
+    return 0;
+}
